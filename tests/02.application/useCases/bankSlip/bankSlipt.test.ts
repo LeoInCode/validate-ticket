6fa42d4b76f1ee9d8b7c6830251bf21d185d8575
Leonardo Souza Mendes <leonardo.mendes@compasso.com.br>
2022-04-10 15:58:34 -0300

    ensure BankSplit return false if code does not contain only numbers

--- tests/02.application/useCases/bankSlip/bankSlipt.test.ts
+++ tests/02.application/useCases/bankSlip/bankSlipt.test.ts
@@ -3,6 +3,10 @@ class BankSlip {
 
   public validate(code: string) {
     if (code.length !== 47) return false;
+
+    if (Number.isNaN(+code)) return false;
+
+    return true;
   }
 }
 
@@ -13,9 +17,17 @@ const makeSut = () => {
 };
 
 describe('BankSlip UseCase ', () => {
-  test('Shoud return false when does not have 47 caracteres', async () => {
+  test('Shoud return false when code does not have 47 caracteres', async () => {
     const { bankSlip } = makeSut();
     const isValid = bankSlip.validate('any_code');
+    expect(isValid).toBe(false);
+  });
+
+  test('Shoud return false when code does not contain only numbers', async () => {
+    const { bankSlip } = makeSut();
+    const isValid = bankSlip.validate(
+      'any_code123458764520394875643210947365287563985',
+    );
     expect(isValid).toBe(false);
   });
 });
