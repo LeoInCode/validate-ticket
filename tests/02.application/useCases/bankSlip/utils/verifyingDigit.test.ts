bbe37d1f7c71923ab74687ab400a4201a2068ba8
Leonardo Souza Mendes <leonardo.mendes@compasso.com.br>
2022-04-10 18:27:31 -0300

    ensure VerifyingDigit return true when sum of code and verifying digit are equal

--- tests/02.application/useCases/bankSlip/utils/verifyingDigit.test.ts
+++ tests/02.application/useCases/bankSlip/utils/verifyingDigit.test.ts
@@ -7,9 +7,17 @@ const makeSut = () => {
 };
 
 describe('VerifyingDigit UseCase ', () => {
-  test('Shoud return false when code when sum of code and verifying digit is not equal', async () => {
+  test('Shoud return false when sum of code and verifying digit is not equal', async () => {
     const { verifiyingDigit } = makeSut();
     const isValid = verifiyingDigit.verifyDigitInBarcode('9832098');
     expect(isValid).toBe(false);
+  });
+
+  test('Shoud return true when sum of code and verifying digit are equal', async () => {
+    const { verifiyingDigit } = makeSut();
+    const isValid = verifiyingDigit.verifyDigitInBarcode(
+      '23799755200003700003381260007827139500006330',
+    );
+    expect(isValid).toBe(true);
   });
 });
