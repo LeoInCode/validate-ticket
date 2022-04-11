388ee8c233eb10090157ca3dfd71d341e9437ee1
Leonardo Souza Mendes <leonardo.mendes@compasso.com.br>
2022-04-11 20:01:28 -0300

    ensure VeryfyingDigitDealership return false when sum of code and veifying digit is not equal

---
+++ tests/02.application/useCases/dealershipSlip/utils/helpers/verifyingDigitDealership.test.ts
@@ -0,0 +1,41 @@
+import VerifyingDigitDealership from '../../../../../../src/02.application/useCases/dealershipSlip/utils/helpers/verifyingDigitDealership';
+
+const makeCodeCalculator = () => {
+  class CodeCalculatorSpy {
+    public getRemainderOfSummation(block: string) {
+      const code = block.split('').reverse();
+      let multiplier = 2;
+      const limitOfMultiplier = 9;
+      const summation = code.reduce((acc, cur) => {
+        const sum = +cur * multiplier;
+        multiplier = multiplier === limitOfMultiplier ? 2 : multiplier + 1;
+        return acc + sum;
+      }, 0);
+
+      const numberOfPositions = 11;
+      const remainder = summation % numberOfPositions;
+
+      return remainder;
+    }
+  }
+
+  const codeCalculator = new CodeCalculatorSpy();
+  return codeCalculator;
+};
+
+const makeSut = () => {
+  const codeCalculatorSpy = makeCodeCalculator();
+  const verifiyingDigitDealership = new VerifyingDigitDealership(
+    codeCalculatorSpy,
+  );
+
+  return { verifiyingDigitDealership };
+};
+
+describe('VerifyingDigitDealership Helper ', () => {
+  test('Should return false when sum of code and verifying digit is not equal', async () => {
+    const { verifiyingDigitDealership } = makeSut();
+    const isValid = verifiyingDigitDealership.verifyDigitInBarcode('9832098');
+    expect(isValid).toBe(false);
+  });
+});
