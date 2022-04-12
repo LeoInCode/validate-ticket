97f3cb03634fa80f02baa715585e6ca273a079c6
Leonardo Souza Mendes <leonardo.mendes@compasso.com.br>
2022-04-12 01:04:27 -0300

    ensure FetlockController return 400 because length of code by BankSlip

--- tests/01.api/fetlockController.spec.ts
+++ tests/01.api/fetlockController.spec.ts
@@ -12,4 +12,11 @@ describe('Get informations about /boleto/', () => {
     );
     expect(result.body.expirationDate).toBe('2018-07-16');
   });
+  test('should return 400 because length of code by BankSlip', async () => {
+    const result = await supertest(app).get(
+      '/boleto/21290001192110001210904475617405975809870000002000',
+    );
+    expect(result.status).toBe(400);
+    expect(result.body.message).toBe('Different length: 47');
+  });
 });
