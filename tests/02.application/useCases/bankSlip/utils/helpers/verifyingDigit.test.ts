765a225758e230bf275f8a8f12f8ab15fc489305
Leonardo Souza Mendes <leonardo.mendes@compasso.com.br>
2022-04-11 21:29:29 -0300

    rename same tests do fix name

--- tests/02.application/useCases/bankSlip/utils/helpers/verifyingDigit.test.ts
+++ tests/02.application/useCases/bankSlip/utils/helpers/verifyingDigit.test.ts
@@ -31,13 +31,13 @@ const makeSut = () => {
 };
 
 describe('VerifyingDigit Helper ', () => {
-  test('Shoud return false when sum of code and verifying digit is not equal', async () => {
+  test('Should return false when sum of code and verifying digit is not equal', async () => {
     const { verifiyingDigit } = makeSut();
     const isValid = verifiyingDigit.verifyDigitInBarcode('9832098');
     expect(isValid).toBe(false);
   });
 
-  test('Shoud return true when sum of code and verifying digit are equal', async () => {
+  test('Should return true when sum of code and verifying digit are equal', async () => {
     const { verifiyingDigit } = makeSut();
     const isValid = verifiyingDigit.verifyDigitInBarcode(
       '23799755200003700003381260007827139500006330',
